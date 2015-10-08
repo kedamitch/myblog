5964598168e726438f9bff4b97e9a386fe56d1d9
yefeizhang <yefeizhang@pptv.com>
2015-10-08 18:40:33 +0800

    cc

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -89,7 +89,7 @@
 
 
     $('#addpost_btn').on('click', function() {
-        window.location.href = '/blog/newPost.html';
+        window.location.href = '/blog/newPost.do';
     });
 
     //初始化页面，获取用户权限信息
